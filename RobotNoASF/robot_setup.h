feb88e97595a302cfd8d6f5dda6438aab22cd932
Mansel Jeffares <Manselj@gmail.com>
2018-05-05 10:15:20 +1200

    Removed redundant proximity data structure

--- RobotNoASF/robot_setup.h
+++ RobotNoASF/robot_setup.h
@@ -210,14 +210,6 @@ typedef struct ColourSensorData
 	uint16_t value;
 } ColourSensorData;
 
-//Stores proximity data
-typedef struct ProximityData
-{
-	uint8_t errorCount;					//Counts the number of times proximity sensors have failed
-	uint8_t pollEnabled;				//Whether or not to poll for new messages in main()
-	uint16_t pollInterval;				//Interval at which to poll at (ms)
-} ProximityData;
-
 //Will store states of the line sensors. This is necessary
 //because there is a gray area when the sensor is half on and half off the line, so by establishing
 //hysteresis and only changing the stored states when an upper and lower threshold is crossed,
